5128019be7365d6c61c1b30a8df2c32dab61d83c
Mostafizur Rahman <mostafizzfuad@gmail.com>
2024-01-31 14:02:09 +0600

    total aizu solved : 08

---
+++ aizu/Range.cpp
@@ -0,0 +1,18 @@
+// created by mostafizzfuad
+// problem link : https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_2_B
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a < b && b < c) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+
+}
