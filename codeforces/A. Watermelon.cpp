19b94f5f5ad9cf0d0e9fb3818c5d3deb9840c365
Mostafizur Rahman <mostafizzfuad@gmail.com>
2024-05-09 12:16:36 +0600

    total codeforces solved : 01

---
+++ codeforces/A. Watermelon.cpp
@@ -0,0 +1,11 @@
+// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int w;
+    cin >> w;
+
+    if (w % 2 == 1 || w == 2) cout << "NO" << endl;
+    else cout << "YES" << endl;
+}
