eca407f1c0503617e4d15b8479e0609cd81a342e
Mostafizur Rahman <mostafizzfuad@gmail.com>
2024-05-20 22:36:20 +0600

    total codeforces solved : 05

---
+++ codeforces/A. Domino piling.cpp
@@ -0,0 +1,11 @@
+// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+
+    int ans = (m * n) / 2;
+    cout << ans << endl;
+}
