3b96c3e43f56bee23da2ddffcf5b3075cd4f9c1e
Mostafizur Rahman <mostafizzfuad@gmail.com>
2024-05-24 00:36:35 +0600

    total codeforces solved : 21

---
+++ codeforces/A. Word.cpp
@@ -0,0 +1,29 @@
+// created by mostafizzfuad
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int upper = 0, lower = 0;
+    string s;
+    cin >> s;
+
+    // count upper and lower
+    for (int i = 0; i < s.length(); i++) 
+    {
+        if (isupper(s[i])) upper++;
+        else lower++;
+    }
+
+    if (lower >= upper) {
+        for (int i = 0; i < s.length(); i++) {
+            s[i] = tolower(s[i]);
+        }
+    } 
+    else {
+        for (int i = 0; i < s.length(); i++) {
+            s[i] = toupper(s[i]);
+        }
+    }
+
+    cout << s << endl;
+}
