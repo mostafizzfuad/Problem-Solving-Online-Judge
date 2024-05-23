89860de906dba944d695db04a56cae28a281c051
Mostafizur Rahman <mostafizzfuad@gmail.com>
2024-05-23 21:08:52 +0600

    total codeforces solved : 14

---
+++ codeforces/A. String Task.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s, ss;
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+
+        if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y')
+        {
+            continue;
+        }
+        else
+        {
+            ss += ".";
+            ss += tolower(s[i]);
+        }
+    }
+
+    cout << ss << endl;
+}
